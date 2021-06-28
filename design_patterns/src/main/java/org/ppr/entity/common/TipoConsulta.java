d01e573c661b7ea731ba14ac7cb5ffd9d6d47a84
Jeferson Souza (thejefecomp) <jefecomp.official@gmail.com>
2021-06-28 04:27:59 +0000

    Updating source-code by cleaning up unnecessary classes; Changing source code level to Java 16.

--- design_patterns/src/main/java/org/ppr/entity/common/TipoConsulta.java
+++ design_patterns/src/main/java/org/ppr/entity/common/TipoConsulta.java
@@ -31,8 +31,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  */
- 
-package org.ppr.entity;
+package org.ppr.entity.common;
  
  
 public enum TipoConsulta{
